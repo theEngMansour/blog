c87f8f29aace8655c47f3f091f282f0c5c896d2a
Mansour Ahmed <73757452+theEngMansour@users.noreply.github.com>
2022-05-11 21:41:49 +0300

    upload avater for user

---
+++ middleware/upload.js
@@ -0,0 +1,24 @@
+import nextConnect from 'next-connect';
+import multer from 'multer';
+
+const DIR = './public/images';
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: DIR,
+        filename: (req, file, cb) => {
+            const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+            cb(null, `${uniqueSuffix}-${file.originalname}`);
+        }
+    })
+});
+
+const apiRoute = nextConnect({
+  onError(error, req, res) {
+    res.status(501).json({ error: `Sorry something Happened! ${error.message}` });
+  },
+  onNoMatch(req, res) {
+    res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
+  },
+});
+
+export {apiRoute, upload}
--- pages/api/account/upload-photo.js
+++ pages/api/account/upload-photo.js
@@ -1,12 +1,31 @@
-import prisma from 'lib/prisma';
 import auth from 'middleware/authentication';
+import prisma from 'lib/prisma';
+import {apiRoute, upload} from 'middleware/upload';
 
-async function handler(req, res) {
-    try {
-        res.status(200).json(req.user)
+apiRoute.use(upload.single('avatar'));
+
+apiRoute.post(async (req, res) => {
+    try{
+        const uploadPhoto = await prisma.user.update({
+            data: {
+                img_uri: '/public/images/' + req.file.filename
+            },
+            where: {
+                id: req.user.id
+            }
+        });
+        res.status(200).json({
+            message: "تم إضافة الصورة بنجاح"
+        });
     } catch(e) {
-        res.status(500).json(e)
+        res.status(500).json(e.message)
     }
-}
+});
 
-export default auth(handler)
+export default auth(apiRoute);
+
+export const config = {
+  api: {
+    bodyParser: false, 
+  },
+};
