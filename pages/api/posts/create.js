df53b64ee2cfd856b8497349df4381ac937122b4
Mansour Ahmed <73757452+theEngMansour@users.noreply.github.com>
2022-05-13 19:44:56 +0300

    create post and upload image

--- pages/api/posts/create.js
+++ pages/api/posts/create.js
@@ -1,15 +1,16 @@
 import prisma from 'lib/prisma';
 import auth from 'middleware/authentication';
 import langs from 'site-settings/site-translations';
-import { validationResult } from 'express-validator';
-import { postValidationRules } from 'middleware/validtor';
+import {apiRoute, upload} from 'middleware/upload';
+import {validationResult} from 'express-validator';
+import {postValidationRules} from 'middleware/validtor';
 
-async function handler(req, res) {
+apiRoute.use(upload.array('postImg', 5));
+apiRoute.post(async (req, res) => {
     if (req.method !== 'POST') return res.status(400).json()
     await postValidationRules(req, res)
     const errors = validationResult(req)
     if (!errors.isEmpty()) return res.status(422).json({ errors: errors.array() })
-    
     const {title, contents, steps, country, region} = req.body;
     try {
         const post = await prisma.post.create({
@@ -22,10 +23,24 @@ async function handler(req, res) {
                 userId: req.user.id
             }
         });
+        req.files.map(async (file) => {
+            const post_img = await prisma.post_Image.create({
+                data: {
+                    img_uri: `/images/${file.filename}`,
+                    postId: post.id
+                }
+            })
+        })
         res.status(200).json({message: langs.ar.createPost});
     } catch(e) {
         res.status(500).json(e)
     }
-}
+});
 
-export default auth(handler)
+export default auth(apiRoute);
+
+export const config = {
+  api: {
+    bodyParser: false, 
+  },
+};
