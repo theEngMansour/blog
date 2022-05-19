4446cd0daed1eca7f58716dc8ae43bb5bc59ed17
Mansour Ahmed <73757452+theEngMansour@users.noreply.github.com>
2022-05-19 11:35:58 +0300

    all the route done

--- layouts/partials/Header.js
+++ layouts/partials/Header.js
@@ -1,43 +1,8 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
-import Image from 'next/image';
-
-const pages = ['الرئيسية', 'التصنيفات', 'الإعلامين'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+import React from 'react';
 
 const ResponsiveAppBar = () => {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
   return (
-    <div></div>
+    <h1>Home</h1>
   );
 };
 export default ResponsiveAppBar;
