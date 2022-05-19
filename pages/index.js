c54b0ca5ce4b5990512f7b7ec7938ddccf68a894
Mansour Ahmed <73757452+theEngMansour@users.noreply.github.com>
2022-05-19 11:34:34 +0300

    index

--- pages/index.js
+++ pages/index.js
@@ -5,20 +5,7 @@ import { MainLayout } from 'layouts';
 export default function Home() {
   return (
     <MainLayout>
-      <Box
-        className="text-center w-[100%] md:w-[90%] lg:w-[70%] m-auto"
-        sx={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          '& > :not(style)': {
-            height: '100%'
-          },
-        }}
-      >
-        <Paper className="shadow-lg mt-6 pb-24 pt-16" variant={0}>
-          <h2 className="text-center m-0 text-[30px]">أهلاً بالعالم 🚀👋</h2>
-        </Paper>
-      </Box>
+      <h1>contant</h1>
     </MainLayout>
   )
 }
