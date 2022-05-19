6e11f378841c49296498fde206091459f84f228a
Mansour Ahmed <73757452+theEngMansour@users.noreply.github.com>
2022-05-19 11:31:45 +0300

    change some ditals

--- layouts/Main.js
+++ layouts/Main.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Container, Paper, Box, Typography, Divider } from '@mui/material';
+import { Container} from '@mui/material';
 import Header from './partials/Header';
-import NavBar from './partials/NavBar';
 import Footer from './partials/Footer';
 
 export default function Main({children}) {
     return (
         <React.Fragment>
             <Header/>
-            <NavBar/>
             <Container  component="main" sx={{my: 0.1}}>
                 {children}
             </Container>
