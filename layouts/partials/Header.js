723ab618448c0f374d752af95a72c959ecd97e1a
Mansour Ahmed <73757452+theEngMansour@users.noreply.github.com>
2022-05-30 15:11:54 +0300

    add header

--- layouts/partials/Header.js
+++ layouts/partials/Header.js
@@ -1,9 +1,40 @@
 import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import Image from 'next/image';
+import {
+  AppBar, 
+  Toolbar, 
+  CssBaseline, 
+  Slide,
+  Button,
+  useScrollTrigger
+} from '@mui/material';
+
 import { AuthContext } from 'layouts/AuthContext';
 import { useRouter } from 'next/router';
 import { Storage } from '@capacitor/storage';
 
-export default function Header() {
+function HideOnScroll(props) {
+  const { children, window } = props;
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+  });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
+HideOnScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  window: PropTypes.func,
+};
+
+
+
+export default function Header(props) {
   const {setLoggedIn} = useContext(AuthContext)
   const router = useRouter()
 
@@ -15,6 +46,25 @@ export default function Header() {
 
   return (
     <React.Fragment>
+      <CssBaseline />
+      <HideOnScroll {...props}>
+        <AppBar sx={{bgcolor: 'white'}} variant={0}>
+          <Toolbar className="flex flex-col">
+            <div className="flex justify-center w-[100%] select-none" variant={0}>
+              <Image src="/logo/logokoora.svg" width={'150px'} height={'90px'} alt="logo"/>
+            </div>
+          </Toolbar>
+          <Toolbar>
+            <div className="flex justify-center w-[100%] select-none" variant={0}>
+              <Button sx={{mx: 1}} className="bg-[#45b97c] text-white">الرئيسية</Button>
+              <Button sx={{mx: 1, color: '#5d5f63'}} className="bg-[#ffffff]">التصنيفات</Button>
+              <Button sx={{mx: 1, color: '#5d5f63'}} className="bg-[#ffffff]">الإعلامين</Button>
+              <Button sx={{mx: 1, color: '#5d5f63'}} className="bg-[#ffffff]">حول</Button>
+              <Button sx={{mx: 1, color: '#5d5f63'}} className="bg-[#ffffff]">الملف الشخصي</Button>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
       <p className="underline selection:bg-slate-500 selection:text-red-200" onClick={logout}>logout</p>
     </React.Fragment>
   );
