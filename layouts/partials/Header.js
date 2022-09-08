fd7e2ad2da70891a0dbc937d04e067c6af321de8
Mansour Ahmed <73757452+theEngMansour@users.noreply.github.com>
2022-09-09 01:14:25 +0300

    added

--- layouts/partials/Header.js
+++ layouts/partials/Header.js
@@ -1,39 +1,67 @@
 import { FormattedMessage } from 'react-intl';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation, Autoplay } from 'swiper';
+import Image from 'next/image';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
+// text-[#faaf40]
+// text-[#d70133]
 export default function Header() {
+
+  const swiper_settings = {
+    navigation: true,
+    pagination: {
+        clickable: true
+    },
+    autoplay: {
+        delay: 3000,
+    }
+  }
+
   return (
   <section className="relative bg-white overflow-hidden bg-[url('/svg/pattern-white.svg')] bg-center">
     <div className="py-20 md:py-28">
       <div className="container px-4 mx-auto">
         <div className="flex flex-wrap xl:items-center -mx-4">
-          <div className="w-full md:w-1/2 px-4 mb-16 md:mb-0">
-            <h1 className="text-3xl md:text-5xl lg:text-6xl leading-tight font-bold tracking-tight m-0">
-              <FormattedMessage id={'header.title'}/>
+          <div className="md:w-[44%] p-1 sm:p-0">
+            <h1 className="text-[46.5px] md:text-5xl lg:text-6xl selection:bg-[#d70133] selection:text-white leading-tight font-bold tracking-tight m-0">
+              <FormattedMessage id={'header.title1'}/>
+              <span className="bg-[#d70133] text-white"> <FormattedMessage id={'header.title2'} /></span>
             </h1>
-            <h1 className="text-3xl md:text-5xl lg:text-6xl leading-tight font-bold tracking-tight m-0 my-2 text-green-500">
+            <h1 className="text-[46.5px] md:text-5xl lg:text-6xl selection:bg-[#faaf40] selection:text-white leading-tight font-bold tracking-tight m-0 my-2 text-[#d70133]">
               <FormattedMessage id={'header.subtitle'}/>
             </h1>
-            <p style={{ fontFamily: "Montserrat-Light"}} className="mb-8 text-lg md:text-xl text-gray-500 font-light">
+            <p style={{ fontFamily: "Montserrat-Light"}} className="mb-8 text-[16.5px] md:text-xl text-gray-500 font-light selection:text-white selection:bg-[#faaf40]">
               <FormattedMessage id={'header.desc'}/>
             </p>
             <div className="flex flex-wrap">
-              <div className="w-full md:w-auto md:mr-4 mx-2">
-                <span className="inline-block py-3 pointer-events-none px-7 w-full text-base md:text-lg leading-4 text-green-50 font-medium text-center bg-green-500 hover:bg-green-600 focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 border border-green-500 rounded-md shadow-sm">
+              <div className="w-full mb-3 md:mb-0 md:w-auto md:mr-4 mx-2">
+                <span className="inline-block py-3 pointer-events-none px-7 w-full text-base md:text-lg leading-4 text-white font-medium text-center bg-[#d70133] hover:bg-[#d70133d7] focus:ring-2 focus:ring-[#d70133] focus:ring-opacity-50 border border-[#d70133] rounded-md shadow-sm">
                   <FormattedMessage id={'header.login'}/>
                 </span>
               </div>
               <div className="w-full md:w-auto md:mr-4 mx-2">
-                <span className="inline-block py-3 pointer-events-none px-7 w-full text-coolGray-800 md:text-lg leading-4 font-medium text-center bg-white hover:bg-coolGray-100 focus:ring-2 focus:ring-coolGray-200 focus:ring-opacity-50 border border-coolGray-200 rounded-md shadow-sm">
+                <span className="inline-block py-3  pointer-events-none px-7 w-full text-coolGray-800 md:text-lg leading-4 font-medium text-center bg-[#faaf40] hover:bg-[#faaf40] focus:ring-2 focus:ring-[#faaf40] focus:ring-opacity-50 border border-coolGray-200 rounded-md shadow-sm">
                 <FormattedMessage id={'title.register'}/>
                 </span>
               </div>
             </div>
           </div>
           <div className="w-full md:w-1/2 px-4">
-            <div className="relative mx-auto md:mr-0 max-w-max">
-              {/* <img className="absolute z-10 -left-14 -top-12 w-28 md:w-auto" src="flex-ui-assets/elements/circle3-yellow.svg" alt=""></img>
-              <img className="absolute z-10 -right-7 -bottom-8 w-28 md:w-auto" src="flex-ui-assets/elements/dots3-blue.svg" alt=""></img>
-              <img className="relative rounded-7xl" src="flex-ui-assets/images/headers/header.jpg" alt=""></img> */}
+            <div className="mt-4 sm:mt-0">
+              <Swiper {...swiper_settings} modules={[Pagination, Navigation, Autoplay]}>   
+{/*                 <SwiperSlide> 
+                  <Image src={'/foods/1.jpg'} width={'3508'} height={'2492'} alt={'food'} />
+                </SwiperSlide>
+                <SwiperSlide> 
+                  <Image src={'/foods/2.jpg'} width={'3508'} height={'2492'} alt={'food'} />
+                </SwiperSlide>
+                <SwiperSlide> 
+                  <Image src={'/foods/3.jpg'} width={'3508'} height={'2492'} alt={'food'} />
+                </SwiperSlide> */}
+              </Swiper>
             </div>
           </div>
         </div>
